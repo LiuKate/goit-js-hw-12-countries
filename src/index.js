dbb15cb901386f8a80ee5ff8170826c2b7fbd184
Kateryna Liubchyk <shevtsova.kateryna@gmail.com>
2021-04-24 22:03:27 +0300

    adding fetchCountries

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './styles.css';
+
+import './js/fetchCountries';
+import './js/countriesMarkup';
+
+
+// fetch(`https://restcountries.eu/rest/v2/name/can`)
+//     .then(res => { return res.json() })
+//     .then(data => {
+//         const markUp = countriesListTpl(data);
+//         console.log(markUp)
+//     })
